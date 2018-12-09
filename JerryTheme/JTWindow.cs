5a229fb13bb43121598274cbada92400db2ffd42
Jerry Zielonka <jerryzielonka@hotmail.com>
2018-12-09 19:00:13 +0100

    Project Upgrade to .Net Framework 4.5 (testing Windows.Shell)

--- JerryTheme/JTWindow.cs
+++ JerryTheme/JTWindow.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
+using System.Windows.Shapes;
+
 
 namespace JerryTheme
 {
@@ -17,10 +22,102 @@ namespace JerryTheme
     /// </summary>
     public class JTWindow : Window
     {
+        /*     EXTERNAL IMPORTS AND ASSOCIATED STUFF       */
+        /***************************************************/
+        /// <summary>
+        /// Import of SendMessage Win API method from user32.dll - for sending the Window Resize message to the system
+        /// </summary>
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern IntPtr SendMessage(IntPtr hWnd, UInt32 msg, IntPtr wParam, IntPtr lParam);
+        /// <summary>
+        /// Window Hock for Win API (user32.dll)
+        /// </summary>
+        private HwndSource _hwndSource;
+        /// <summary>
+        /// ResizeDirection Enumeration for Window Resizing
+        /// </summary>
+        private enum ResizeDirection
+        {
+            Left = 1,
+            Right = 2,
+            Top = 3,
+            TopLeft = 4,
+            TopRight = 5,
+            Bottom = 6,
+            BottomLeft = 7,
+            BottomRight = 8,
+        }
+
+        /*     WINDOW GENERAL SETUP        */
+        /***********************************/
         /// <summary>
         /// Set DefaultStyleKeyProperty for all instances of JTWindow - for Style and Template purposes
         /// </summary>
         static JTWindow() => DefaultStyleKeyProperty.OverrideMetadata(typeof(JTWindow), new FrameworkPropertyMetadata(typeof(JTWindow)));
+        /// <summary>
+        /// JTWindow Constructor
+        /// </summary>
+        public JTWindow() : base()
+        {
+            PreviewMouseMove += JTWindow_OnPreviewMouseMove;
+        }
+        /// <summary>
+        /// <para>OnApplyTemplate OVERRIDE</para>
+        /// <para>for attaching click handlers to Header Buttons</para>
+        /// </summary>
+        public override void OnApplyTemplate()
+        {
+            // Header Click for Window DragMove and Maximize/Restore on DoubleClick Handler
+            if (GetTemplateChild("brdWindowHeader") is Border brdWindowHeader)
+            {
+                brdWindowHeader.MouseDown += Header_MouseDown;
+            }
+
+            // Header Button Handlers for Window Minimize, Maximize/Restore and Close actions
+            if (GetTemplateChild("btnMinimize") is Button btnMinimize)
+            {
+                btnMinimize.Click += MinimizeButton_Click;
+            }
+            if (GetTemplateChild("btnMaximizeRestore") is Button btnMaximizeRestore)
+            {
+                btnMaximizeRestore.Click += MaximizeRestoreButton_Click;
+            }
+            if (GetTemplateChild("btnClose") is Button btnClose)
+            {
+                btnClose.Click += CloseButton_Click;
+            }
+
+            // Window resize with hocks handlers
+            if (GetTemplateChild("grdResizeHocks") is Grid grdResizeHocks)
+            {
+                foreach (UIElement hock in grdResizeHocks.Children)
+                {
+                    if (hock is Rectangle resizeHock)
+                    {
+                        resizeHock.PreviewMouseDown += ResizeHock_PreviewMouseDown;
+                        resizeHock.MouseMove += ResizeHock_MouseMove; ;
+                    }
+                }
+            }
+
+            // run base
+            base.OnApplyTemplate();
+        }
+        /// <summary>
+        /// OnInitialized OVERRIDE to get the SourceInitialized event hock
+        /// </summary>
+        protected override void OnInitialized(EventArgs e)
+        {
+            this.SourceInitialized += OnSourceInitialized;
+            base.OnInitialized(e);
+        }
+        /// <summary>
+        /// SourceInitialized Handler to set the Window Hock for resizing purposes
+        /// </summary>
+        private void OnSourceInitialized(object sender, EventArgs e)
+        {
+            _hwndSource = (HwndSource)PresentationSource.FromVisual(this);
+        }
 
 
         /*     HEADER BACKGROUND BRUSH        */
@@ -38,50 +135,141 @@ namespace JerryTheme
             set { SetValue(HeaderBarBackgroundProperty, value); }
         }
 
+
+        /*     WINDOW HANDLING VIA MOUSE      */
+        /**************************************/
         /// <summary>
-        /// <para>OnApplyTemplate OVERRIDE</para>
-        /// <para>For attaching click handlers to Header Buttons</para>
+        /// Window DragMove and Maximize/Restore actions when Header is Clicked/DoubleClicked
         /// </summary>
-        public override void OnApplyTemplate()
+        private void Header_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
-            if (GetTemplateChild("btnMinimize") is Button btnMinimize)
+            if (e.ClickCount > 1 && e.ChangedButton == System.Windows.Input.MouseButton.Left)
             {
-                btnMinimize.Click += Window_Minimize;
+                this.WindowState = (this.WindowState == WindowState.Normal) ? WindowState.Maximized : WindowState.Normal;
             }
-
-            if (GetTemplateChild("btnMaximizeRestore") is Button btnMaximizeRestore)
+            if (e.ChangedButton == System.Windows.Input.MouseButton.Left && e.LeftButton == MouseButtonState.Pressed)
             {
-                btnMaximizeRestore.Click += Window_MaximizeRestore;
+                this.DragMove();
             }
-
-            if (GetTemplateChild("btnClose") is Button btnClose)
-            {
-                btnClose.Click += Window_Close;
-            }
-
-            base.OnApplyTemplate();
         }
-
         /// <summary>
         /// Minimize Window action
         /// </summary>
-        protected void Window_Minimize(object sender, EventArgs e)
+        private void MinimizeButton_Click(object sender, EventArgs e)
         {
             this.WindowState = WindowState.Minimized;
         }
         /// <summary>
         /// Maximize/Restore Window action
         /// </summary>
-        protected void Window_MaximizeRestore(object sender, EventArgs e)
+        private void MaximizeRestoreButton_Click(object sender, EventArgs e)
         {
             this.WindowState = (this.WindowState == WindowState.Normal) ? WindowState.Maximized : WindowState.Normal;
         }
         /// <summary>
         /// Close Window action
         /// </summary>
-        protected void Window_Close(object sender, EventArgs e)
+        private void CloseButton_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+        /// <summary>
+        /// Bring back the default arrow cursor when the mouse moves over the window and left mouse button is not pressed
+        /// </summary>
+        private void JTWindow_OnPreviewMouseMove(object sender, MouseEventArgs e)
+        {
+            if (Mouse.LeftButton != MouseButtonState.Pressed)
+            {
+                Cursor = Cursors.Arrow;
+            }
+        }
+        /// <summary>
+        /// Change cursor to appropriate Sizing cursor direction depending on the resizeHock sending the event
+        /// </summary>
+        private void ResizeHock_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            Rectangle rectangle = sender as Rectangle;
+            switch (rectangle.Name)
+            {
+                case "top":
+                    Cursor = Cursors.SizeNS;
+                    break;
+                case "bottom":
+                    Cursor = Cursors.SizeNS;
+                    break;
+                case "left":
+                    Cursor = Cursors.SizeWE;
+                    break;
+                case "right":
+                    Cursor = Cursors.SizeWE;
+                    break;
+                case "topLeft":
+                    Cursor = Cursors.SizeNWSE;
+                    break;
+                case "topRight":
+                    Cursor = Cursors.SizeNESW;
+                    break;
+                case "bottomLeft":
+                    Cursor = Cursors.SizeNESW;
+                    break;
+                case "bottomRight":
+                    Cursor = Cursors.SizeNWSE;
+                    break;
+                default:
+                    break;
+            }
+        }
+        /// <summary>
+        /// Perform the Resize operation depending on the resizeHock sending the event
+        /// </summary>
+        private void ResizeHock_PreviewMouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            Rectangle rectangle = sender as Rectangle;
+            switch (rectangle.Name)
+            {
+                case "top":
+                    Cursor = Cursors.SizeNS;
+                    ResizeWindow(ResizeDirection.Top);
+                    break;
+                case "bottom":
+                    Cursor = Cursors.SizeNS;
+                    ResizeWindow(ResizeDirection.Bottom);
+                    break;
+                case "left":
+                    Cursor = Cursors.SizeWE;
+                    ResizeWindow(ResizeDirection.Left);
+                    break;
+                case "right":
+                    Cursor = Cursors.SizeWE;
+                    ResizeWindow(ResizeDirection.Right);
+                    break;
+                case "topLeft":
+                    Cursor = Cursors.SizeNWSE;
+                    ResizeWindow(ResizeDirection.TopLeft);
+                    break;
+                case "topRight":
+                    Cursor = Cursors.SizeNESW;
+                    ResizeWindow(ResizeDirection.TopRight);
+                    break;
+                case "bottomLeft":
+                    Cursor = Cursors.SizeNESW;
+                    ResizeWindow(ResizeDirection.BottomLeft);
+                    break;
+                case "bottomRight":
+                    Cursor = Cursors.SizeNWSE;
+                    ResizeWindow(ResizeDirection.BottomRight);
+                    break;
+                default:
+                    break;
+            }
+        }
+        /// <summary>
+        /// Sends the Resize Event Message to the system with the Window Hock
+        /// </summary>
+        /// <param name="direction">ResizeDirection in which the window should be resized</param>
+        private void ResizeWindow(ResizeDirection direction)
+        {
+            SendMessage(_hwndSource.Handle, 0x112, (IntPtr)(61440 + direction), IntPtr.Zero);
         }
     }
 }
--- JerryThemeTest/TestWindow.xaml.cs
+++ JerryThemeTest/TestWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -9,26 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
-using jt = JerryTheme;
 
 namespace JerryThemeTest
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for TestWindow.xaml
     /// </summary>
-    public partial class MainWindow : jt.JTWindow
+    public partial class TestWindow : Window
     {
-        public MainWindow()
+        public TestWindow()
         {
             InitializeComponent();
-        }
-
-        private void BtnDUPA_Click(object sender, RoutedEventArgs e)
-        {
-            //ScreenHandler 
-            //Screen[] screens = Screen.AllScreens;
         }
     }
 }
